680dd25cd0997376a952a5a29b68a77edf5f861a
JeremyFabrizioPerdomoM <jeperdomo864@unicah.edu>
2026-05-26 13:11:05 -0600

    Delete PerfilPortafolio directory

--- PerfilPortafolio/cssjs/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const form = document.getElementById("formContacto");
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Gracias por tu mensaje, pronto me pondré en contacto contigo.");
-    form.reset();
-  });
-});
